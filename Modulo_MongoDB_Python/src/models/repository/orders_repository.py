dbb18e415c2793da8237754f825e491326298960
pedrou103 <pedroluca13.plm40@gmail.com>
2025-12-02 16:34:48 -0300

    feat: part de fundamentos do Mongo DB no python

---
+++ Modulo_MongoDB_Python/src/models/repository/orders_repository.py
@@ -0,0 +1,73 @@
+from bson.objectid import ObjectId
+
+
+class OrdersRepository:
+    def __init__(self, db_connection) -> None:
+        self.__collection_name = "orders"
+        self.__db_connection = db_connection
+
+    def insert_document(self, document: dict) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.insert_one(document)
+
+    def insert_list_of_documents(self, list_of_documents: list) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.insert_many(list_of_documents)
+
+    def select_many(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_filter)
+        return data
+
+    def select_one(self, doc_filter: dict) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find_one(doc_filter)
+        return data
+
+    def select_many_with_properties(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(
+            doc_filter, {"_id": 0, "cupom": 0}  # filtro de busca  # Opções de retorno
+        )
+        return data
+
+    def select_if_property_exists(self) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find({"address": {"$exists": True}}, {"_id": 0, "itens": 0})
+        return data
+
+    def select_by_object_id(self, object_id: str) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find_one({"_id": ObjectId(object_id)})
+        return data
+
+    def edit_registry(self) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.update_one(
+            {"_id": ObjectId("692eff8001a2d01bf2f9060f")},  # Filtros
+            {"$set": {"itens.pizza.quantidade": 50}},  # Edição
+        )
+
+    def edit_many_registry(self) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.update_many(
+            {"itens.refrigerante": {"$exists": True}},  # Filtros
+            {"$set": {"itens.pizza.quantidade": 50}},  # Edição
+        )
+
+    def edit_registry_with_increment(self) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.update_one(
+            {"_id": ObjectId("692eff8001a2d01bf2f9060f")},  # Filtros
+            {"$inc": {"itens.pizza.quantidade": 50}},  # Edição
+        )
+
+    def delete_registry(self) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.delete_one(
+            {"_id": ObjectId("692f0d5f9f94a9726f3e93c7")},  # Filtros
+        )
+
+    def delete_many_registries(self) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.delete_many({"itens.refrigerante": {"$exists": True}})
